027b14538c6fedf466eda3e74d67228d349d35d6
Sumit <kingsumit011@gmail.com>
2021-11-03 23:55:11 +0530

    Youtube transcript Api implemented

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from datetime import datetime
+from youtube_transcript_api import YouTubeTranscriptApi
 
 # define a variable to hold you app
 app = Flask(__name__)
@@ -12,6 +13,14 @@ def index_page():
 app.route('/time', methods=['GET'])
 def get_time():
     return str(datetime.datetime.now())
+
+def youtube_vedio_transcript_english(vedio_id:str):
+    transcript_en = YouTubeTranscriptApi.get_transcript(vedio_id , languages=['en'])
+    transcript = ""
+    for item in transcript_en:
+        transcript += item["text"]
+    return transcript
+
 
 # server the app when this file is run
 if __name__ == '__main__':
