3bd090dcf3cc3440dcdffb1c9db205340eb10166
Sumit <kingsumit011@gmail.com>
2021-11-05 16:02:19 +0530

    make transcript summarization function using NLP

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from datetime import datetime
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
 from youtube_transcript_api import YouTubeTranscriptApi
 
 # define a variable to hold you app
@@ -21,6 +23,25 @@ def youtube_vedio_transcript_english(vedio_id:str):
         transcript += item["text"]
     return transcript
 
+def transcript_summarization(transcript:str):
+
+    # initialize the model architecture and weights
+    model = T5ForConditionalGeneration.from_pretrained("t5-large")
+    # initialize the model tokenizer
+    tokenizer = T5Tokenizer.from_pretrained("t5-large")  
+    # encode the text into tensor of integers using the appropriate tokenizer
+    inputs = tokenizer.encode("summarize: " + transcript, return_tensors="pt", max_length=1024, truncation=True)
+    # generate the summarization output
+    outputs = model.generate(
+    inputs, 
+    max_length=250, 
+    min_length=100, 
+    length_penalty=2.0, 
+    num_beams=4, 
+    early_stopping=True)
+
+    summary =tokenizer.decode(outputs[0])
+    return summary
 
 # server the app when this file is run
 if __name__ == '__main__':
